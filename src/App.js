fb4b7f90ff285f206957926f1bce787318be31a5
sja31dev <sja31dev+bitbucket@gmail.com>
2017-11-10 00:37:39 +0000

    Access flickr API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-
 
 import Header from './components/header';
 import Footer from './components/footer';
 import PhotoStream from './components/photostream';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      stream: null
+    };
+  }
+
+  componentDidMount() {
+    this.getMorePhotos();
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,6 +29,31 @@ class App extends Component {
         <Footer />
       </div>
     );
+  }
+
+  getMorePhotos() {
+    axios.get("https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json&nojsoncallback=true")
+      .then((response) => {
+        console.log(response.data.items);
+      /*  this.setState({
+            items: response.data.items
+        })*/
+      })
+      .catch((err) => {
+      console.log(err)
+      })
+    /*const url = "http://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true";
+    fetch(url, {method: 'get'})
+    .then(results => {
+      console.log(results);
+      return results.json();
+    }).then( data => {
+      if (data.error) {
+        alert("Sorry there was an error retrieving photos from flickr")
+      } else {
+        console.log(data);
+      }
+    });*/
   }
 }
 
